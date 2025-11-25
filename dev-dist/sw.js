187ef34f4f3d8631b2f136b754985cc474cb10b1
Chinaezurum <ndumelechinaezurum@gmail.com>
2025-11-25 15:36:14 +0100

    fix: rebuild production bundle to resolve JavaScript initialization error

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hdvsshti12o"
+    "revision": "0.jrr6pfbciq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
